29731951aa0409e4a6f0cc3b2747ba57b30eb28f
AditiAgarwal <84237896+aditiagarwal30@users.noreply.github.com>
2021-06-07 00:27:11 +0530

    !

---
+++ __main__ & __name__/two.py
@@ -0,0 +1,12 @@
+#two.py
+
+def func():
+	print('FUNC() in TWO.PY')
+
+print('TOP LEVEL IN TWO.PY')
+
+if __name__ == '__main__':
+		print('TWO.PY IS BEING RUN DIRECTLY !!')
+else:
+         print('TWO.PY HAS BEEN IMPORTED')
+         			
